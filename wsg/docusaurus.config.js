a40a0c76ae8d02c0014e20b79fea165cbd333a44
wastelandsurvivalguide <liamtoye@gmail.com>
2023-03-11 22:15:02 -0500

    asdf

--- wsg/docusaurus.config.js
+++ wsg/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
     url: "https://wastelandsurvivalguide.com",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/wastelandsurvivalguide",
+    baseUrl: "/wastelandsurvivalguide/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
