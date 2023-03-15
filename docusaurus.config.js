b979bcd06adc78310c486dc733135c27195e8104
lctoye <lctoye@myseneca.ca>
2023-03-15 05:54:42 -0400

    add "edit this page" links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,13 +45,13 @@ const config = {
                     sidebarPath: require.resolve("./sidebars.js"),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/wastelandsurvivalguide/wastelandsurvivalguide.github.io/edit/main/',
                 },
                 blog: {
                     showReadingTime: false,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    //editUrl: 'https://github.com/wastelandsurvivalguide/wastelandsurvivalguide.github.io/tree/main/',
                 },
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
