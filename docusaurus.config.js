8717b6934009621f828c5297a2e7f29cb66a2b50
Yorman Florez <126834772+n3xSU5@users.noreply.github.com>
2026-08-01 16:42:58 -0500

    feat: remove unused styles and add new ones for accesibility

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
           onUntruncatedBlogPosts: 'warn',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: './src/css/survivalguru.css',
         },
       }),
     ],
